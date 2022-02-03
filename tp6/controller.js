5bbc197b0ad0c0ac41c1a862856a65aef11cce3b
Thomas Orgeval <orgevalthomas@gmail.com>
2022-02-03 22:36:29 +0100

    tp6

--- tp6/controller.js
+++ tp6/controller.js
@@ -1,17 +1,30 @@
-
-var editingMode = { rect: 0, line: 1 };
+const editingMode = {rect: 0, line: 1}
 
 function Pencil(ctx, drawing, canvas) {
-	this.currEditingMode = editingMode.line;
-	this.currLineWidth = 5;
-	this.currColour = '#000000';
-	this.currentShape = 0;
+  this.currEditingMode = editingMode.line
+  this.currLineWidth = 5
+  this.currColour = document.getElementById('colour').value
+  this.currentShape = 0
 
-	// Liez ici les widgets à la classe pour modifier les attributs présents ci-dessus.
+  // Liez ici les widgets à la classe pour modifier les attributs présents ci-dessus.
+  new DnD(canvas, this)
 
-	new DnD(canvas, this);
+  // Implémentez ici les 3 fonctions onInteractionStart, onInteractionUpdate et onInteractionEnd
+  this.onInteractionStart = function (x0, y0, x1, y1) {
 
-	// Implémentez ici les 3 fonctions onInteractionStart, onInteractionUpdate et onInteractionEnd
-};
+  }
 
+  this.onInteractionUpdate = function (x0, y0, x1, y1) {
+
+  }
+
+  this.onInteractionEnd = function (x0, y0, x1, y1) {
+    const shape = (this.currentShape === editingMode.line ?
+        new Ligne(x0, y0, x1, y1, document.getElementById("spinnerWidth").value, document.getElementById("colour").value) :
+        new Rectangle(x0, y0, x1, y1, document.getElementById("spinnerWidth").value, document.getElementById("colour").value)
+    )
+    drawing.addForm(shape)
+    drawing.paint(ctx)
+  }
+}
 
--- tp6/view.js
+++ tp6/view.js
@@ -1,2 +1,26 @@
-
 // Implémenter ici les fonctions paint à ajouter dans chacune des classes du modèle.
+Rectangle.prototype.paint = function (ctx) {
+//TODO Manager color
+  ctx.fillStyle = this.color
+  ctx.rect(this.x0, this.y0, this.x1, this.y1)
+  ctx.stroke()
+}
+
+Ligne.prototype.paint = function (ctx) {
+//TODO Manager color
+  ctx.fillStyle = this.color
+  ctx.beginPath()
+  ctx.moveTo(this.x0, this.y0)
+  ctx.lineTo(this.x1, this.y1)
+  ctx.stroke()
+}
+
+Drawing.prototype.paint = function (ctx) {
+  console.log(this.getForms())
+  ctx.fillStyle = '#F0F0F0' // set canvas' background color
+  ctx.fillRect(0, 0, canvas.width, canvas.height)
+  this.getForms().forEach(function (t) {
+    // now fill the canvas
+    t.paint(ctx)
+  })
+}
