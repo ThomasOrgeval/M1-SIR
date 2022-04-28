ba3586bc50ffa414f0eaaed8fb6a3fc4a86a8db0
Thomas Orgeval <orgevalthomas@gmail.com>
2022-04-28 06:05:22 +0200

    start with database

--- tp5-quarkus/src/main/resources/import.sql
+++ tp5-quarkus/src/main/resources/import.sql
@@ -12,7 +12,7 @@ values (1, '2022-05-01', '3', 'Ajouter des tags', 'Fonction utile et agréable p
        (2, '2022-05-05', '4', 'Carte de test', '', 'Espagne', 'TODO', '', 1, 1),
        (3, '2022-05-01', '1', 'Vérification des méthodes utilisées', '-GET, -POST, -PUT, -DELETE', 'Rennes, France',
         'TODO', 'http://localhost:8080/api/', 2, 2),
-       (4, '2022-05-01', '1', 'Note maximale 20/20 😋', '', 'Rennes, France', 'DONE', '', 2, 2),
+       (4, '2022-05-01', '1', 'Avoir aimé le projet 😋', '', 'Rennes, France', 'DONE', '', 2, 2),
        (5, '2022-05-01', '1', 'Usage de technos récentes', '', 'Rennes, France', 'DOING', '', 2, 2),
        (6, '2022-05-01', '1', 'Regarder le TP7', 'Un besoin de rendre le site un peu plus complet et joli!',
         'Rennes, France',
